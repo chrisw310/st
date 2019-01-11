e1b510ba6d04f4f05b684b792ed307bda7d65c6f
Chris Wilkinson <cw@fea.st>
2019-01-11 20:33:39 +1000

    smaller text

--- config.h
+++ config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "mono:pixelsize=45:antialias=true:autohint=true";
+static char *font = "mono:pixelsize=40:antialias=true:autohint=true";
 static int borderpx = 100;
 
 /*
